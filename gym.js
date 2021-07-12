fb9aee2fd8f84f9d0d2d483b1bcf76671be0171d
zaini-rosman <81604263+zaini-rosman@users.noreply.github.com>
2021-07-12 16:17:52 +0800

    Add files via upload

---
+++ gym.js
@@ -0,0 +1,22 @@
+const express = require ('express')
+const  mongoose = require('mongoose')
+const app = express()
+
+const port = process.env.PORT || 1919
+
+mongoose.connect("mongodb+srv://m001-student:m001-mongodb-basics@sandbox.ehulh.mongodb.net/gymnasium?retryWrites=true&w=majority" , {
+useNewParser: true,
+useUnifiedTopology : true
+}, () => {
+console.log('Connected to mongodb atlas') 
+})
+
+
+
+app.get('/',(req,res) => {
+    res.send('Hi Zaini')
+})
+
+app.listen(process.env.PORT,( ) => {
+    console.log('Connected to the port')
+})
